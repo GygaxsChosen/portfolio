f5f34103c17d9c9957a5b34c59179b4d6d976239
GygaxsChosen <deal4131@gmail.com>
2017-06-12 16:23:59 -0400

    commit

---
+++ Bank/src/Accounts/Account.java
@@ -0,0 +1,23 @@
+package Accounts;
+
+public class Account
+
+{
+public int AccountNum;
+public int AccountPin;
+public double AccountBalance;
+
+
+	public double getBal()
+	{
+		return AccountBalance;
+	}
+	public void SetBal(double input)
+	{
+		AccountBalance=input;
+		
+	}
+	
+}
+
+
---
+++ Bank/src/Accounts/Main.java
@@ -0,0 +1,10 @@
+package Accounts;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("wellcome to ConsoleLine banking");
+
+}
+	}
